6d76e4c29a28f3e572b05458062b2448b7cdbc43
angelachan64 <angelachan64@gmail.com>
2016-05-22 22:25:31 +0000

    I forgot to push?

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ import os.path
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return render_template("home.html")
+def index():
+    return render_template("index.html")
 
 @app.route("/login")
 def login():
     return render_template("login.html")
+    
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
    app.debug = True
