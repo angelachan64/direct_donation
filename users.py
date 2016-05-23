ac5770acc31ebc8cd0c83312700867e238a62737
Dillon Zhang <d1998z@gmail.com>
2016-05-22 21:35:01 -0400

    db fn

--- users.py
+++ users.py
@@ -41,9 +41,50 @@ def valid_login(username, password):
     q = 'SELECT password, salt FROM organization WHERE username = ?'
     pepper_and_salt = c.execute(q, (username,)).fetchone()
     if pepper_and_salt and sha512((password + pepper_and_salt[1]) * 10000).hexdigest() == pepper_and_salt[0]:
-        q = "SELECT parent_id FROM organization WHERE username = ?"
+        q = "SELECT user_id FROM organization WHERE username = ?"
         id = c.execute(q, (username,)).fetchone()
         conn.close()
         return id[0]
     conn.close()
     return -1
+
+
+def get_orgs():
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+    q = 'SELECT name FROM sqlite_master WHERE TYPE = "table" AND NAME = "organization"'
+    c.execute(q)
+    if not c.fetchone():
+        conn.close()
+        return {}
+    q = 'SELECT user_id, username FROM organization'
+    organs = c.execute(q)
+    returner = {}
+    for i in range(len(organs)):
+        returner[organs[i][0]] = organs[i][1]
+    return returner
+
+
+def make_donation(time, first, last, amount, email, organ_id):
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+    q = 'CREATE TABLE IF NOT EXISTS donors (time DATETIME, first TEXT, last TEXT, amount INT, email TEXT, organ_id INT)'
+    c.execute(q)
+    q = 'INSERT INTO donors (time, first, last, amount, email, organ_id) VALUES (?, ?, ?, ?, ?, ?)'
+    c.execute(q, (time, first, last, amount, email, organ_id))
+    conn.commit()
+    conn.close()
+    return True
+
+
+def get_donation(organ_id):
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+    q = 'SELECT name FROM sqlite_master WHERE TYPE = "table" AND NAME = "donors"'
+    c.execute(q)
+    if not c.fetchone():
+        conn.close()
+        return {}
+    q = 'SELECT * FROM donors WHERE organ_id = ?'
+    donations = c.execute(q, (organ_id,))
+    return donations
