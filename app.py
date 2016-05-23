6b7a71c9304854927301d95e1f29c18a77e1dc30
TESLA <57leonardo@gmail.com>
2016-05-22 20:31:06 -0400

    fake payment form

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session
 from flask import redirect, url_for
+from flask import request, session, redirect
 import os.path
 
 app = Flask(__name__)
@@ -15,6 +16,33 @@ def login():
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route("/payment", methods=['GET', 'POST'])
+def payment():
+    if request.method=="GET":
+        return render_template("payment.html")
+    else:
+        #THE INFO COLLECTED FOR PAYMENT
+        session["name"] = request.form["name"]
+        session["amount"] = request.form["amount"]
+        session["email"] = request.form["email"]
+        session["date"] = request.form["date"]
+        return redirect(url_for("transactions"))
+
+@app.route("/transactions")
+def transactions():
+    n = session["name"]
+    a = session["amount"]
+    e = session["email"]
+    d = session["date"]
+    return render_template("transactions.html", 
+                           name = n,
+                           amount = a,
+                           email = e,
+                           date = d
+                       )
+    
+
 
 if __name__ == "__main__":
    app.debug = True
