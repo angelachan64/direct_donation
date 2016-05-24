43861fa1353c4299e483de25b63f31a114b66c56
angelachan64 <angelachan64@gmail.com>
2016-05-24 13:48:32 +0000

    some things

---
+++ static/index.js
@@ -0,0 +1,13 @@
+console.log("begin");
+
+$("#login").click(function login(){
+    $(".left").fadeIn(1000);
+    $(".right").fadeOut(1000);
+})
+
+$("#signup").click(function signup(){
+    $(".left").fadeOut(1000);
+    $(".right").fadeIn(1000);
+})
+
+console.log("end");
