86add20aa54ba9c2eb3ffd76d1c20bae6e1118b0
angelachan64 <angelachan64@gmail.com>
2016-05-24 14:06:40 +0000

    pulling removed my changes

--- static/index.js
+++ static/index.js
@@ -3,11 +3,11 @@ console.log("begin");
 $("#login").click(function login(){
     $(".left").fadeIn(1000);
     $(".right").fadeOut(1000);
-})
+});
 
 $("#signup").click(function signup(){
     $(".left").fadeOut(1000);
     $(".right").fadeIn(1000);
-})
+});
 
 console.log("end");
