75b18ade5df96aa9a1ebde828a32946e49a7eccf
TESLA <57leonardo@gmail.com>
2016-06-10 01:50:08 -0400

    things still work, getting emails

--- static/emails.js
+++ static/emails.js
@@ -1,6 +1,32 @@
-var colArray = $('#paypal-stats td:nth-child(3)').map(function(){
-    return $(this).text();
-}).get()​;
+//console.log("hi");
 
-console.log(colArray)
 
+var oTable = document.getElementById('paypal-stats');
+//console.log("oTable:");
+//console.log(oTable);
+
+// var rowLength = oTable.rows.length;
+// for (i = 0; i < rowLength; i++){
+//     var oCells = oTable.rows.item(i).cells;
+//     var cellLength = oCells.length;
+//     // for(var j = 0; j < cellLength; j++){
+//     //     var cellVal = oCells.item(j).innerHTML;
+//     //     console.log(cellVal);
+//     // }
+//     for(var j = 0; j < cellLength; j++){
+// 	var cellVal = oCells.item(j).innerHTML;
+// 	console.log(cellVal);
+//     }
+// }
+
+for (var i = 0; i < oTable.rows.length; i++) {
+    var oCells = oTable.rows.item(i).cells;
+    //for (var j = 0; j < oCells.length; j++) {
+    EMAILCOL=8;
+    j=EMAILCOL;
+	var cellVal = oCells.item(j).innerHTML;
+	console.log(cellVal);
+    //}
+}
+
+//console.log("bye");
